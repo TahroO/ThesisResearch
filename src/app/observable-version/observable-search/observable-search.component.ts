ab062cb25ac79f27d39b21b8da3cab21b27d15cb
Bernd <bernd.reusch@hotmail.com>
2025-05-27 21:33:42 +0200

    refactor(search) some comments - small refactoring to signals - adds subscription handling to observable version

--- src/app/observable-version/observable-search/observable-search.component.ts
+++ src/app/observable-version/observable-search/observable-search.component.ts
@@ -1,5 +1,5 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {BehaviorSubject, combineLatest, map, Observable} from 'rxjs';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {BehaviorSubject, combineLatest, map, Observable, Subscription} from 'rxjs';
 import {Product} from '../../model/product';
 import {ProductService} from '../../service/productService';
 import {AsyncPipe, NgForOf} from '@angular/common';
@@ -15,8 +15,12 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './observable-search.component.html',
   styleUrl: './observable-search.component.css'
 })
-export class ObservableSearchComponent implements OnInit {
+/**
+ * Represents an observable version of a product search component
+ */
+export class ObservableSearchComponent implements OnInit, OnDestroy {
   private productService = inject(ProductService);
+  private subscription = new Subscription();
 
   protected searchTerm = "";
   protected availability = false;
@@ -30,14 +34,16 @@ export class ObservableSearchComponent implements OnInit {
   protected filteredProducts$: Observable<Product[]> | undefined;
   protected categories: string[] = [];
 
-  constructor() {};
+  constructor() {
+  };
 
   // makes sure all data is available as this is async --> combineLatest
   ngOnInit(): void {
     this.products$ = this.productService.getProducts();
-    this.products$.subscribe(products => {
+    const subscribedProducts = this.products$.subscribe(products => {
       this.categories = [...new Set(products.map(p => p.category))];
     });
+    this.subscription.add(subscribedProducts);
     this.filteredProducts$ = combineLatest([
       this.products$,
       this.appliedSearchTerm,
@@ -45,7 +51,7 @@ export class ObservableSearchComponent implements OnInit {
       this.appliedCategory
     ]).pipe(
       map(([products, term, onlyAvailable, selectedCategory]) => {
-        console.warn(`filteredProducts$ Observable recomputed`);
+        console.log(`filteredProducts$ Observable recomputed`);
         return products.filter(product =>
           product.name.toLowerCase().includes(term.toLowerCase()) &&
           (!onlyAvailable || product.available) &&
@@ -54,7 +60,11 @@ export class ObservableSearchComponent implements OnInit {
       })
     );
   }
-  applyFilter() {
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  protected applyFilter() {
     this.appliedSearchTerm.next(this.searchTerm);
     this.appliedAvailability.next(this.availability);
     this.appliedCategory.next(this.category);
--- src/app/signal-version/signal-search/signal-search.component.ts
+++ src/app/signal-version/signal-search/signal-search.component.ts
@@ -3,6 +3,7 @@ import {Product} from '../../model/product';
 import {ProductService} from '../../service/productService';
 import {NgForOf} from '@angular/common';
 import {FormsModule} from '@angular/forms';
+import {toSignal} from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-signal-search',
@@ -13,38 +14,47 @@ import {FormsModule} from '@angular/forms';
   templateUrl: './signal-search.component.html',
   styleUrl: './signal-search.component.css'
 })
+/**
+ * Represents a signal version of a product search component
+ */
 export class SignalSearchComponent {
   private productService = inject(ProductService);
 
-  searchTerm = "";
-  availability = false;
-  category = "";
+  // local states ui-bindings
+  protected searchTerm = "";
+  protected availability = false;
+  protected category = "";
 
+  // applied filter states after button event
   protected appliedSearchTerm = signal<string>("");
   protected appliedAvailability = signal<boolean>(false);
   protected appliedCategory = signal<string>("");
-
-  protected products = signal<Product[]>([]);
-  protected categories = signal<string[]>([]);
-
   constructor() {
-    this.productService.getProducts().subscribe(products => {
-      this.products.set(products);
-      this.categories.set([...new Set(products.map(p => p.category))]);
-    });
-  };
+  }
 
+  // gathered data resources from http request
+  protected products = toSignal(
+    this.productService.getProducts(),
+    {initialValue: [] as Product[]}
+  );
+
+  protected categories = computed(() => [
+    ...new Set(this.products().map(p => p.category))
+  ]);
+
+  // this triggers once on initialization and afterward only if a filter signal changed
   protected filteredProducts = computed(() => {
     const term = this.appliedSearchTerm().toLowerCase();
-    console.warn(`filteredProducts Signal recomputed`);
+    console.log(`filteredProducts Signal recomputed`);
     return this.products().filter(product =>
-    product.name.toLowerCase().includes(term) &&
+      product.name.toLowerCase().includes(term) &&
       (!this.appliedAvailability() || product.available) &&
       (this.appliedCategory() === "" || product.category === this.appliedCategory())
     )
   });
 
-  applyFilter() {
+  // applies filter values from template - set() only executes after internal equal() === false
+  protected applyFilter() {
     this.appliedSearchTerm.set(this.searchTerm);
     this.appliedAvailability.set(this.availability);
     this.appliedCategory.set(this.category);
