87f9b44b8c636b50c89f1b5718bf6858862c5a30
Bernd <bernd.reusch@hotmail.com>
2025-06-04 09:47:05 +0200

    adds first observable form structure

--- src/app/observable-version/observable-form/observable-form.component.ts
+++ src/app/observable-version/observable-form/observable-form.component.ts
@@ -1,11 +1,90 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import {debounceTime, filter, map, Observable, startWith, Subject, switchMap, takeUntil} from 'rxjs';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {User} from '../../model/user';
+import {UserService} from '../../service/userService';
+import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-observable-form',
-  imports: [],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    NgIf,
+    AsyncPipe,
+    NgForOf
+  ],
   templateUrl: './observable-form.component.html',
   styleUrl: './observable-form.component.css'
 })
-export class ObservableFormComponent {
+export class ObservableFormComponent implements OnInit, OnDestroy{
+
+  private destroy$ = new Subject<void>();
+  private userService = inject(UserService);
+
+  protected userForm: FormGroup;
+
+  protected userNameStatus$: Observable<string>;
+  protected emailStatus$: Observable<string>;
+  protected formFieldStatus$: Observable<string>;
+
+  protected userSearchResults: User[] | undefined;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.userForm = this.formBuilder.group({
+
+      username: ["", [Validators.required, Validators.minLength(3)]],
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      emailAddress: ["", [Validators.required, Validators.email]],
+      address: ["", Validators.required],
+      phoneNumber: ["", [Validators.required, Validators.pattern(/^[0-9]+$/)]],
+
+      }
+    )
+    this.userNameStatus$ = this.userForm.controls['username'].valueChanges.pipe(
+      debounceTime(300),
+      filter(value => value.length > 2),
+      switchMap(value => this.userService.isNameTaken(value)),
+      map(isTaken => (isTaken ? 'Name existiert bereits' : 'ok')),
+      startWith('')
+    );
+
+    this.emailStatus$ = this.userForm.controls['emailAddress'].valueChanges.pipe(
+      debounceTime(300),
+      filter(value => value.includes('@')),
+      switchMap(value => this.userService.isEmailTaken(value)),
+      map(isTaken => (isTaken ? 'E-Mail existiert bereits' : 'ok')),
+      startWith('')
+    );
+
+    // Formularstatus
+    this.formFieldStatus$ = this.userForm.statusChanges.pipe(startWith(this.userForm.status));
+
+  }
+
+
+
+  ngOnInit(): void {
+    this.userForm.controls['username'].valueChanges.pipe(
+      debounceTime(400),
+      filter(value => value.length > 2),
+      switchMap(value => this.userService.searchUsers(value)),
+      takeUntil(this.destroy$)
+    ).subscribe(results => {
+      this.userSearchResults = results;
+    });
+  }
+
+  onSubmit() {
+
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 
 }
---
+++ src/app/service/userService.ts
@@ -0,0 +1,40 @@
+import {User} from '../model/user';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map, Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  protected users: Observable<User[]>;
+  constructor(private http: HttpClient) {
+    this.users = this.http.get<User[]>('/data/users.json')
+  }
+
+  isNameTaken(name: string) {
+    return this.users.pipe(
+      map(users =>
+        users.some(user => user.userName.toLowerCase() === name.toLowerCase())
+      )
+    );
+  }
+
+
+  isEmailTaken(address: string) {
+    return this.users.pipe(
+      map(users =>
+        users.some(user => user.eMailAddress.toLowerCase() === address.toLowerCase())
+      )
+    );
+  }
+
+  searchUsers(name: string): Observable<User[]> {
+    return this.users.pipe(
+      map(users => users.filter(user =>
+        user.userName.toLowerCase().includes(name.toLowerCase())
+      ))
+    );
+  }
+}
